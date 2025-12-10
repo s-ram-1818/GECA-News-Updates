f35838ff9b1944eeeff8eb3554c552a3de630bba
s-ram-1818 <ramsarwade1818@gmail.com>
2025-12-10 23:18:33 +0530

    fix

--- main.js
+++ main.js
@@ -30,7 +30,9 @@ app.use(express.static("public"));
 
 // --- Mail Transporter ---
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
